f21643fdc2d670bea36d4eeff68758225d4373b2
Tim Schulz <tim@getbej.com>
2022-01-26 08:53:45 +0000

    Refactor the update worksheet functions into one function

--- run.py
+++ run.py
@@ -28,7 +28,7 @@ def get_sales_data():
         sales_data = data_str.split(",")
         
         if validate_data(sales_data):
-            print("Data is valid")
+            print("Data is valid\n")
             break
     return sales_data
 
@@ -49,23 +49,15 @@ def validate_data(values):
         return False
     return True
 
-def update_sales_data(data):
+def update_worksheet(data, worksheet):
     """
-    Update sales worksheet, add new row with the data provided.
+    Receives a list of integers to be inserted into the worksheet.
+    Update the relevant worksheet with the data provided.
     """
-    print("Updating sales worksheet...")
-    sales_worksheet = SHEET.worksheet("sales")
-    sales_worksheet.append_row(data)
-    print("Sales worksheet updated successfully.\n")
-
-def update_surplus_data(data):
-    """
-    Update the surplus worksheet, add a new row with the calculated surplus data.
-    """
-    print("Updating surplus worksheet...\n")
-    surplus_worksheet = SHEET.worksheet("surplus")
-    surplus_worksheet.append_row(data)
-    print("Surplus worksheet updated successfully.\n")
+    print(f"Updating {worksheet.capitalize()} worksheet...\n")
+    worksheet_to_update = SHEET.worksheet(worksheet)
+    worksheet_to_update.append_row(data)
+    print(f"{worksheet.capitalize()} worksheet updated successfully.\n")
 
 def calculate_surplus_data(sales_row):
     """Compare sales with stock and calculate the surplus for each item type
@@ -91,9 +83,9 @@ def main():
 
     data = get_sales_data()
     sales_data = [int(num) for num in data]
-    update_sales_data(sales_data)
+    update_worksheet(sales_data, "sales")
     new_surplus_data = calculate_surplus_data(sales_data)
-    update_surplus_data(new_surplus_data)
+    update_worksheet(new_surplus_data, "surplus")
     
 
 print("Welcome to LoveSandwiches data automation!\n")
