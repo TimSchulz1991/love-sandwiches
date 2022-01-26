5f82afe367774e7ebd804f429106b56e925577fb
Tim Schulz <tim@getbej.com>
2022-01-26 12:45:34 +0000

    Build a function to grab a list of the past 5 sales values for each sandwich

--- run.py
+++ run.py
@@ -78,6 +78,20 @@ def calculate_surplus_data(sales_row):
     
     return surplus_data
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet, collects the last 5 entries 
+    for each sandwich and return the data as a list of lists.
+    """
+
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range (1,7):
+        column = sales.col_values(ind)[-5:]
+        columns.append(column)
+    
+    return columns
+
 def main():
     """Run all program functions"""
 
@@ -89,4 +103,6 @@ def main():
     
 
 print("Welcome to LoveSandwiches data automation!\n")
-main()
+#main()
+
+sales_columns = get_last_5_entries_sales()
